312880a5552ee52886fd81413476b93dc986670b
LeonardoSilva <apx88.0@gmail.com>
2020-06-10 18:10:36 -0400

    bootstrap test

--- yak-yik/src/App.js
+++ yak-yik/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Button from 'react-bootstrap/Button'
 function App() {
   return (
     <div className="App">
-    
+    <Button variant="primary">Primary</Button>
     </div>
   );
 }
