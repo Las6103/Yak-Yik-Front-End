359a95763aa6eebf3f23de37c71e308225a50b69
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-06-11 17:52:56 -0400

    worked on modal for updating meme

--- yak-yik/src/Memepage.js
+++ yak-yik/src/Memepage.js
@@ -12,6 +12,7 @@ class Memepage extends Component {
     this.state = {
       data: { image_url: "" },
       showDelete: false,
+      showUpdate: false
     };
   }
 
@@ -43,6 +44,20 @@ class Memepage extends Component {
     });
   };
 
+  update = () => {
+    return axios({
+      method: "put",
+      url: `https://yak-yik-api.herokuapp.com/memes/id/${this.state.data.image_url}`,
+    });
+  };
+
+  handleUpdate = () => {
+    this.update().then(() => {
+      this.setState({ showDelete: false });
+      this.props.history.push("/memes");
+    })
+  }
+
   /**
    * Modal Controls
    */
@@ -55,6 +70,16 @@ class Memepage extends Component {
     this.setState({ showDelete: false });
   };
 
+  handleShowUpdate = () => {
+    this.setState({ showUpdate: true });
+  };
+
+  handleCloseUpdate = () => {
+    this.setState({ showUpdate: false});
+  };
+
+  
+
   render() {
     console.log(this.state.data);
     const data = this.state.data;
@@ -63,6 +88,9 @@ class Memepage extends Component {
         <Image src={data.image_url} />
         <Button variant="danger" onClick={this.handleShowDelete}>
           Delete
+        </Button>
+        <Button variant="warning" onClick={this.handleShowUpdate}>
+          Update
         </Button>
         <Modal
           show={this.state.showDelete}
@@ -74,11 +102,30 @@ class Memepage extends Component {
           </Modal.Header>
           <Modal.Body>You're about to delete this dank meme!</Modal.Body>
           <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleCloseUpdate}>
+              Close
+            </Button>
+            <Button variant="danger" onClick={this.handleUpdate}>
+              Delete
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal
+          show={this.state.showUpdate}
+          onHide={this.handleCloseDelete}
+          animation={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Please Confirm</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>You're about to update this dank meme!</Modal.Body>
+          <Modal.Footer>
             <Button variant="secondary" onClick={this.handleCloseDelete}>
               Close
             </Button>
-            <Button variant="danger" onClick={this.handleDelete}>
-              Delete
+            <Button variant="warning" onClick={this.handleUpdate}>
+              Update
             </Button>
           </Modal.Footer>
         </Modal>
