d9c224ff6785c0c0e5a2024ba97217dd2ffadc2a
LeonardoSilva <apx88.0@gmail.com>
2020-06-11 15:06:25 -0400

    updated feature branch

--- yak-yik/src/Postpage.js
+++ yak-yik/src/Postpage.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
-import Menu from './Menu.js';
-import Replylist from './Replylist.js';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
+import React, { Component } from "react";
+import Menu from "./Menu.js";
+import Replylist from "./Replylist.js";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { Formik } from "formik";
 class Postpage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       data: {
-        post: '',
-        reply: [],
+        post: "",
+        reply: [
+          {
+            reply: "",
+          },
+        ],
       },
     };
   }
@@ -28,9 +38,26 @@ class Postpage extends Component {
         this.setState({ data: parsedData });
       });
   };
+
+  /**
+   * Create Reply
+   */
+  create = (values) => {
+    const currentreplies = this.state.data.reply.map((data) => data.reply);
+    const replyobj = currentreplies.map((str) => ({ reply: str }));
+    const repliesobj = replyobj.push(values);
+    console.log(replyobj);
+    return axios({
+      method: "put",
+      url: `https://yak-yik-api.herokuapp.com/posts/id/${this.state.data._id}`,
+      data: {
+        reply: replyobj,
+      },
+    });
+  };
+
   render() {
     const content = this.state.data;
-    console.log(content);
     return (
       <>
         <Container>
@@ -40,6 +67,45 @@ class Postpage extends Component {
               <Card.Title>{content.post}</Card.Title>
             </Card.Body>
           </Card>
+
+          <Formik
+            initialValues={{
+              reply: "",
+            }}
+            enableReinitialize={true}
+            onSubmit={(values) => {
+              this.create(values).then(() => this.getPost());
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Row>
+                  <Col>
+                    <Form.Group controlId="formGroupReply">
+                      <Form.Label>Reply</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Insert Reply"
+                        controlid="reply"
+                        name="reply"
+                        onChange={props.handleChange}
+                        value={props.values.reply}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    props.handleSubmit();
+                    this.getPost();
+                  }}
+                >
+                  Create
+                </Button>
+              </Form>
+            )}
+          </Formik>
 
           <Replylist data={content.reply} />
         </Container>
