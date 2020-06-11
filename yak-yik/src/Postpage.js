530cfdc751da04f41c1d6d3dc54ce73b7cc742af
LeonardoSilva <apx88.0@gmail.com>
2020-06-11 15:11:39 -0400

    updated menu

--- yak-yik/src/Postpage.js
+++ yak-yik/src/Postpage.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Menu from "./Menu.js";
 import Replylist from "./Replylist.js";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
