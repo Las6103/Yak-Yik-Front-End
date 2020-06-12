f6578753f0f69c5e76fce1fd6de8c276ed443473
LeonardoSilva <apx88.0@gmail.com>
2020-06-11 21:56:41 -0400

    created tests

--- yak-yik/src/Createpost.test.js
+++ yak-yik/src/Createpost.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import App from "./App";
+import Createpost from "./Createpost";
 import { shallow } from "enzyme";
 
 describe("First React component test with Enzyme", () => {
   it("renders without crashing", () => {
-    shallow(<App />);
+    shallow(<Createpost />);
   });
 });
